d6057a7db2c6c8e474710fd70a510d24b7d03eaf
Victor Vanegas <vanegas.victor2001@gmail.com>
2022-03-17 13:45:36 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserMenuComponent } from './components/user-menu/user-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserMenuComponent
   ],
   imports: [
     BrowserModule,
